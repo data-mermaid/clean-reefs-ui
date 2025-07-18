09fe19575507c3ae1026fb2ab19c2ed25be2f2db
Stacy <hurrendor@gmail.com>
2025-07-18 15:11:55 -0700

    C73 Map styling and setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './styles/index.css'
-import App from './App'
+import './styles/index.module.scss'
+import BaseMap from "./BaseMap";
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BaseMap />
   </StrictMode>,
 )
