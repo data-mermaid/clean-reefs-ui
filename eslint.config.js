409afe8cef0eac4315071b3f2de50c25283189a3
Stacy <hurrendor@gmail.com>
2025-07-21 15:53:36 -0700

    C73 Prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,68 +1,71 @@
 // For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
 import storybook from "eslint-plugin-storybook";
 
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { globalIgnores } from 'eslint/config'
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
+import { globalIgnores } from "eslint/config";
 
-export default tseslint.config([
-  globalIgnores(['dist']),
-  {
-    files: ['**/*.{ts,tsx}'],
-    rules: {
-      camelcase: 'off',
-      curly: 'error',
+export default tseslint.config(
+  [
+    globalIgnores(["dist"]),
+    {
+      files: ["**/*.{ts,tsx}"],
+      rules: {
+        camelcase: "off",
+        curly: "error",
 
-      'react/jsx-key': [
-        1,
-        {
-          checkFragmentShorthand: true,
-        },
+        "react/jsx-key": [
+          1,
+          {
+            checkFragmentShorthand: true,
+          },
+        ],
+
+        "no-underscore-dangle": "off",
+        "react/jsx-props-no-spreading": "off",
+        "react/jsx-boolean-value": "off",
+        "react-hooks/rules-of-hooks": "error",
+        "react-hooks/exhaustive-deps": "error", // please never override this rule, even locally
+
+        "no-console": ["error", { allow: ["warn", "error"] }],
+        "no-undef-init": "error",
+
+        "@typescript-eslint/no-unused-vars": [
+          "error",
+          {
+            varsIgnorePattern: "^_",
+            argsIgnorePattern: "^_",
+          },
+        ],
+
+        "no-useless-return": "off",
+
+        "jsx-a11y/label-has-associated-control": [
+          "error",
+          {
+            assert: "either",
+          },
+        ],
+        "@typescript-eslint/no-empty-function": "off",
+        "no-param-reassign": "error",
+        "react/no-danger": "error",
+        "max-nested-callbacks": ["error", { max: 3 }],
+        "consistent-return": "error",
+      },
+      extends: [
+        js.configs.recommended,
+        tseslint.configs.recommended,
+        reactHooks.configs["recommended-latest"],
+        reactRefresh.configs.vite,
       ],
-
-      'no-underscore-dangle': 'off',
-      'react/jsx-props-no-spreading': 'off',
-      'react/jsx-boolean-value': 'off',
-      'react-hooks/rules-of-hooks': 'error',
-      'react-hooks/exhaustive-deps': 'error', // please never override this rule, even locally
-
-      'no-console': ['error', { allow: ['warn', 'error'] }],
-      'no-undef-init': 'error',
-
-      '@typescript-eslint/no-unused-vars': [
-        'error',
-        {
-          varsIgnorePattern: '^_',
-          argsIgnorePattern: '^_',
-        },
-      ],
-
-      'no-useless-return': 'off',
-
-      'jsx-a11y/label-has-associated-control': [
-        'error',
-        {
-          assert: 'either',
-        },
-      ],
-      '@typescript-eslint/no-empty-function': 'off',
-      'no-param-reassign': 'error',
-      'react/no-danger': 'error',
-      'max-nested-callbacks': ['error', { max: 3 }],
-      'consistent-return': 'error',
+      languageOptions: {
+        ecmaVersion: 2020,
+        globals: globals.browser,
+      },
     },
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
-  },
-], storybook.configs["flat/recommended"]);
+  ],
+  storybook.configs["flat/recommended"],
+);
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import React, { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './styles/index.module.scss'
-import BaseMap from './BaseMap'
+import React, { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./styles/index.module.scss";
+import BaseMap from "./BaseMap";
 
-createRoot(document.getElementById('root')!).render(
-    <StrictMode>
-        <BaseMap />
-    </StrictMode>,
-)
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <BaseMap />
+  </StrictMode>,
+);
