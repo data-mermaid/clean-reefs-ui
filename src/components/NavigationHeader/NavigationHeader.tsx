95d6873f93e6cec244772ff66b9c0f458dce4966
tienhoah <nickhoang11@gmail.com>
2025-08-01 11:34:08 -0700

    feat: implement NavigationHeader component with responsive design and internationalization support, and unit test with storybook

--- src/components/NavigationHeader/NavigationHeader.tsx
+++ src/components/NavigationHeader/NavigationHeader.tsx
@@ -1,79 +1,81 @@
-import * as React from 'react'
-import AppBar from '@mui/material/AppBar'
-import Box from '@mui/material/Box'
-import Toolbar from '@mui/material/Toolbar'
-import Typography from '@mui/material/Typography'
-import IconButton from '@mui/material/IconButton'
-import MenuIcon from '@mui/icons-material/Menu'
-import ShareIcon from '@mui/icons-material/Share'
-import { Menu, MenuItem, Link } from '@mui/material'
-import { useTranslation } from 'react-i18next'
-import styles from './NavigationHeader.module.scss'
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import ShareIcon from "@mui/icons-material/Share";
+import { Menu, MenuItem, Link } from "@mui/material";
+import { useTranslation } from "react-i18next";
+import styles from "./NavigationHeader.module.scss";
 
 const menuSx = {
-  '& .MuiPaper-root': {
-    backgroundColor: '#174b82',
+  "& .MuiPaper-root": {
+    backgroundColor: "#174b82",
   },
-}
+};
 
 const menuOriginConfig = {
   anchorOrigin: {
-    vertical: 'bottom' as const,
-    horizontal: 'left' as const,
+    vertical: "bottom" as const,
+    horizontal: "left" as const,
   },
   transformOrigin: {
-    vertical: 'top' as const,
-    horizontal: 'left' as const,
+    vertical: "top" as const,
+    horizontal: "left" as const,
   },
-}
+};
 
 export default function Header() {
-  const { t } = useTranslation()
-  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null)
+  const { t } = useTranslation();
+  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(
+    null,
+  );
 
   const navItems = [
-    { label: t('science_and_methods'), href: '#' },
-    { label: t('contact'), href: '#' },
-  ]
+    { label: t("science_and_methods"), href: "#" },
+    { label: t("contact"), href: "#" },
+  ];
 
   const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorElNav(event.currentTarget)
-  }
+    setAnchorElNav(event.currentTarget);
+  };
 
   const handleCloseNavMenu = () => {
-    setAnchorElNav(null)
-  }
+    setAnchorElNav(null);
+  };
 
   return (
     <Box>
-      <AppBar position="sticky" className={styles['MuiAppBar-root']}>
-        <Toolbar className={styles['MuiToolbar-root']}>
-          <Typography className={styles['logo']}>GPW</Typography>
-          <div className={styles['navigation-container']}>
-            <IconButton aria-label={t('share_view')}>
-              <ShareIcon className={styles['header-icon']} />
+      <AppBar position="sticky" className={styles["MuiAppBar-root"]}>
+        <Toolbar className={styles["MuiToolbar-root"]}>
+          <Typography className={styles["logo"]}>GPW</Typography>
+          <div className={styles["navigation-container"]}>
+            <IconButton aria-label={t("share_view")}>
+              <ShareIcon className={styles["header-icon"]} />
             </IconButton>
-            <Box className={styles['navigation-desktop-menu-box']}>
+            <Box className={styles["navigation-desktop-menu-box"]}>
               {navItems.map((item) => (
                 <Link
                   href={item.href}
                   target="_blank"
                   rel="noopener noreferrer"
-                  className={styles['navigation-item-link']}
+                  className={styles["navigation-item-link"]}
                   key={item.label}
                 >
                   {item.label}
                 </Link>
               ))}
             </Box>
-            <Box className={styles['navigation-mobile-menu-box']}>
+            <Box className={styles["navigation-mobile-menu-box"]}>
               <IconButton
-                aria-label={t('toggle_navigation_menu')}
+                aria-label={t("toggle_navigation_menu")}
                 aria-controls="menu-appbar"
                 aria-haspopup="true"
                 onClick={handleOpenNavMenu}
               >
-                <MenuIcon className={styles['header-icon']} />
+                <MenuIcon className={styles["header-icon"]} />
               </IconButton>
               <Menu
                 id="menu-appbar"
@@ -86,7 +88,9 @@ export default function Header() {
               >
                 {navItems.map((item) => (
                   <MenuItem key={item.label} onClick={handleCloseNavMenu}>
-                    <Typography className={styles['navigation-menu-item']}>{item.label}</Typography>
+                    <Typography className={styles["navigation-menu-item"]}>
+                      {item.label}
+                    </Typography>
                   </MenuItem>
                 ))}
               </Menu>
@@ -95,5 +99,5 @@ export default function Header() {
         </Toolbar>
       </AppBar>
     </Box>
-  )
+  );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
-import React, { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './styles/index.module.scss'
-import '../i18n'
-import { StyledEngineProvider } from '@mui/material'
-import NavigationHeader from './components/NavigationHeader/NavigationHeader'
-import LayersDrawer from './components/LayersDrawer/LayersDrawer'
-import BaseMap from './components/BaseMap/BaseMap'
+import React, { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./styles/index.module.scss";
+import "../i18n";
+import { StyledEngineProvider } from "@mui/material";
+import NavigationHeader from "./components/NavigationHeader/NavigationHeader";
+import LayersDrawer from "./components/LayersDrawer/LayersDrawer";
+import BaseMap from "./components/BaseMap/BaseMap";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <StyledEngineProvider injectFirst>
       <NavigationHeader />
@@ -15,4 +15,4 @@ createRoot(document.getElementById('root')!).render(
       <BaseMap />
     </StyledEngineProvider>
   </StrictMode>,
-)
+);
