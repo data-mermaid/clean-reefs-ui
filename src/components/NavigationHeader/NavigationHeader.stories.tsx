8aab0bac6962d4e2993c828b20abb03fcbf85840
tienhoah <nickhoang11@gmail.com>
2025-08-01 12:55:30 -0700

    C75 fix: prettier autoformat.

--- src/components/NavigationHeader/NavigationHeader.stories.tsx
+++ src/components/NavigationHeader/NavigationHeader.stories.tsx
@@ -1,15 +1,15 @@
-import type { Meta, StoryObj } from "@storybook/react-vite";
+import type { Meta, StoryObj } from '@storybook/react-vite'
 
-import NavigationHeader from "./NavigationHeader";
+import NavigationHeader from './NavigationHeader'
 
 const meta = {
   component: NavigationHeader,
-} satisfies Meta<typeof NavigationHeader>;
+} satisfies Meta<typeof NavigationHeader>
 
-type Story = StoryObj<typeof meta>;
+type Story = StoryObj<typeof meta>
 
 export const Primary: Story = {
   args: {},
-};
+}
 
-export default meta;
+export default meta
