ee33950a82cb820cc824b1e221cd5c80330c0764
Stacy <hurrendor@gmail.com>
2025-09-08 13:30:05 -0700

    C79 Jest test installation

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+export default {
+  restoreMocks: true,
+  transformIgnorePatterns: [
+    '<rootDir>/node_modules/(?!(@iconify-icons|@reach|@fontsource|uuid|dexie)/.*)', // ignore transforming node_modules except for the libraries inside the inner brackets
+  ],
+  moduleNameMapper: {
+    '^.+\\.svg$': 'jest-svg-transformer',
+    '^.+\\.(css|less|scss|png)$': 'identity-obj-proxy',
+    '@fontsource/open-sans': 'identity-obj-proxy',
+  },
+  // testEnvironment: 'jest-environment-jsdom',
+  // setupFiles: ['dotenv/config'],
+  // setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
+  transform: {
+    '^.+\\.[t|j]sx?$': 'babel-jest',
+    '^.+\\.js$': 'babel-jest',
+  },
+}
--- src/tests/updateGraph.test.ts
+++ src/tests/updateGraph.test.ts
@@ -2,17 +2,29 @@
 //test: data is grouped by type
 //test: grouped data is sorted by year
 
-//setGraphData
+//mapGraphAttributes
 //test: data formatted is in the correct format //ts?
 //test: data has a width
 //test: failed/updated/incorrect data throws an error that will populate the page
 
-import { updateGraph } from '../utils/updateGraph'
+import { updateGraph, mapGraphAttributes } from '../utils/updateGraph'
+import mockOutputGraphData from './mockOutputGraphData.json'
+
+const groupedProperties = {
+  bare_ground: { '2000': 5, '2005': 7 },
+  built_up: { '2000': 2 },
+  cropland: { '2005': 7 },
+  high_canopy_forest: { '2010': 3 },
+  mixed_forest: { '2015': 4 },
+  shrubland_grassland: { '2020': 6 },
+  surface_water: { '2000': 1 },
+}
 
 describe('updateGraph', () => {
-  it('groups data by type and year', () => {
+  it('groups data by type and year and filters out unused properties', () => {
     const input = {
       Bare_Gr_pct_2000: 5,
+      Bare_Gr_pct_2005: 7,
       Built_pct_2000: 2,
       Crop_pct_2005: 7,
       HC_Forest_pct_2010: 3,
@@ -25,14 +37,14 @@ describe('updateGraph', () => {
 
     const result = updateGraph(input)
 
-    expect(result).toEqual({
-      bare_ground: { '2000': 5 },
-      built_up: { '2000': 2 },
-      cropland: { '2005': 7 },
-      high_canopy_forest: { '2010': 3 },
-      mixed_forest: { '2015': 4 },
-      shrubland_grassland: { '2020': 6 },
-      surface_water: { '2000': 1 },
-    })
+    expect(result).toEqual(groupedProperties)
+  })
+})
+
+describe('mapGraphAttributes', () => {
+  it('sort values by year within category', () => {
+    const result = mapGraphAttributes(groupedProperties)
+
+    expect(result).toEqual(mockOutputGraphData)
   })
 })
