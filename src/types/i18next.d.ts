10bba95a3db66772de9bae31824ccac4d2d5d066
Stacy <hurrendor@gmail.com>
2025-09-10 11:18:23 -0700

    C79 Code cleanup

--- src/types/i18next.d.ts
+++ src/types/i18next.d.ts
@@ -1,10 +1,13 @@
 import 'i18next'
-import translation from './src/locales/en/translation.json'
+import translation from '../locales/en/translation.json'
 
 declare module 'i18next' {
   interface CustomTypeOptions {
+    defaultNS: 'translation'
     resources: {
       en: typeof translation
     }
+    returnNull: false
+    allowObjectInHTMLChildren: true
   }
 }
