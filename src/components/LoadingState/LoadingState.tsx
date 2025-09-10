6f98c400d973f1367a6ca8819b5d3f96417fb8de
Stacy <hurrendor@gmail.com>
2025-09-10 16:55:26 -0700

    C86 Styling on loading state

--- src/components/LoadingState/LoadingState.tsx
+++ src/components/LoadingState/LoadingState.tsx
@@ -2,28 +2,24 @@ import CircularProgress from '@mui/material/CircularProgress'
 import Typography from '@mui/material/Typography'
 import React from 'react'
 import { useTranslation } from 'react-i18next'
+import styles from './LoadingState.module.scss'
 
-export default function LoadingState() {
+interface LoadingStateProps {
+  height?: number
+}
+
+export default function LoadingState({ height }: LoadingStateProps) {
   const { t } = useTranslation()
 
   return (
     <div
       style={{
-        display: 'block',
-        position: 'relative',
-        padding: '10px',
+        ...(height && { height: `${height}px`, paddingTop: height / 2 }),
       }}
+      className={styles['LoadingState--root']}
     >
       <Typography style={{ textAlign: 'center' }}>{t('loading')}...</Typography>
-      <CircularProgress
-        style={{
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          margin: '0 auto',
-          height: '50px', //rotation causes overflow issues
-        }}
-      />
+      <CircularProgress className={styles['CircularProgress--root']} />
     </div>
   )
 }
