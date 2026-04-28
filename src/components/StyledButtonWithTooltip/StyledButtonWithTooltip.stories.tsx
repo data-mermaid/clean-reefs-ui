c1d5b5169cc9fe73233b97f6a580b9e5b6f2e93c
tienhoah <nickhoang11@gmail.com>
2026-04-28 14:40:05 -0700

    C162: Update button storybook

--- src/components/StyledButtonWithTooltip/StyledButtonWithTooltip.stories.tsx
+++ src/components/StyledButtonWithTooltip/StyledButtonWithTooltip.stories.tsx
@@ -1,6 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react-vite'
 import StyledButtonWithTooltip from './StyledButtonWithTooltip'
 import LayersIcon from '@mui/icons-material/Layers'
+import UpOneLevelIcon from '../../assets/up-one-level.svg'
 import layerStyles from '../LayersDrawer/LayersDrawer.module.scss'
 
 const meta: Meta<typeof StyledButtonWithTooltip> = {
@@ -9,16 +10,18 @@ const meta: Meta<typeof StyledButtonWithTooltip> = {
 
 type Story = StoryObj<typeof StyledButtonWithTooltip>
 
-export const ButtonWithTooltipText: Story = {
+export const ButtonWithTooltip: Story = {
   args: {
+    'aria-label': 'Action button',
     tooltipText: 'Tooltip Text',
     children: <LayersIcon />,
     handleOnClick: () => {},
   },
 }
 
-export const ButtonWithoutTooltipText: Story = {
+export const ButtonWithoutTooltip: Story = {
   args: {
+    'aria-label': 'Action button',
     children: <LayersIcon />,
     handleOnClick: () => {},
   },
@@ -26,10 +29,32 @@ export const ButtonWithoutTooltipText: Story = {
 
 export const LayerToggleButton: Story = {
   args: {
+    'aria-label': 'Open menu',
     tooltipText: 'Open menu',
     children: <LayersIcon />,
     handleOnClick: () => {},
-    className: layerStyles.layerToggleButton,
+    className: layerStyles['layer-toggle-button'],
+  },
+}
+
+export const IconButtonWithTooltip: Story = {
+  args: {
+    isIconButton: true,
+    'aria-label': 'Go up one level',
+    tooltipText: 'Go up one level',
+    tooltipPlacement: 'top',
+    children: <img src={UpOneLevelIcon} alt="" />,
+    handleOnClick: () => {},
+  },
+}
+
+export const IconButtonWithoutTooltip: Story = {
+  args: {
+    isIconButton: true,
+    'aria-label': 'Go up one level',
+    tooltipPlacement: 'top',
+    children: <img src={UpOneLevelIcon} alt="" />,
+    handleOnClick: () => {},
   },
 }
 
